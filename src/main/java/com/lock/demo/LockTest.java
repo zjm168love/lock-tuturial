54bd9426920652918bd1d44336e17d4bbfa80e07
wangtao1 <wangtao1@xiaomi.com>
2017-08-05 17:48:50 +0800

    add spinLock
    Signed-off-by: wangtao1 <wangtao1@xiaomi.com>

---
+++ src/main/java/com/lock/demo/LockTest.java
@@ -0,0 +1,32 @@
+package com.lock.demo;
+
+/**
+ * Created by wangtao on 8/5/17.
+ */
+public class LockTest  implements Runnable {
+    static int sum;
+    private SpinLock lock;
+
+    public LockTest(SpinLock lock) {
+        this.lock = lock;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SpinLock lock = new SpinLock();
+        for (int i = 0; i < 100; i++) {
+            LockTest test = new LockTest(lock);
+            Thread t = new Thread(test);
+            t.start();
+        }
+
+        Thread.currentThread().sleep(1000);
+        System.out.println(sum);
+    }
+
+
+    public void run() {
+        this.lock.lock();
+        sum++;
+        this.lock.unLock();
+    }
+}
